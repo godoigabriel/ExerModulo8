c7c6b4f12f746220bf9ff817a894ed1e38e77471
Gabriel Godoi <103514608+godoigabriel@users.noreply.github.com>
2024-11-26 16:29:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 $(document).ready(function() {
-    // Função para adicionar uma nova tarefa
     $('#task-form').submit(function(event) {
-        event.preventDefault();  // Evita o envio do formulário
+        event.preventDefault();
 
-        // Pega o valor do campo de tarefa
         var taskName = $('#task-name').val();
 
         if (taskName) {
-            // Cria um novo item de lista <li>
             var newTask = $('<li>').text(taskName);
 
-            // Adiciona o item à lista ordenada
             $('#task-list').append(newTask);
 
-            // Limpa o campo de entrada
             $('#task-name').val('');
         }
     });
 
-    // Função para marcar a tarefa como concluída (com riscado)
     $('#task-list').on('click', 'li', function() {
-        $(this).toggleClass('completed');  // Adiciona ou remove a classe 'completed'
+        $(this).toggleClass('completed');
     });
 });
